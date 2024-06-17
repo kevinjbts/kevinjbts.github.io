f8ae5c4ec0cb957657f17a2cd54e52899133bfd1
Kevin Bates <kevinjbts@gmail.com>
2024-06-17 12:15:29 -0500

    Page changes

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,28 +60,19 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Kevin Bates.</h2>;
+  const three = <h3 className="big-heading">Data Analyst & Strategist.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I'm a Data Analyst & strategist who has worked across startups, corporate and other
+        experiences.
       </p>
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+    <a className="email-link" href="https://www.google.com" target="_blank" rel="noreferrer">
+      Check out my Tableau Public!
     </a>
   );
 
